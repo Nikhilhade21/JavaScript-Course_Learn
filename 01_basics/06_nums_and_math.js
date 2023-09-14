b29b6fc07ad825ef9c7dc1cd31a3314811201db8
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-14 19:08:17 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1))
+
+const otherNumber = 1235.456
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++++ maths +++++++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-5));
+console.log(Math.round(4.7));
+console.log(Math.ceil(5.3));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,6,7,2,1,8,5));
+console.log(Math.max(3,4,2,7,8,42,8))
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
