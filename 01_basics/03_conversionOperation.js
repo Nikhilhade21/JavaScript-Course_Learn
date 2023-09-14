e9c5136f289747b0767731be2f45b57b687ebe10
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-14 12:01:24 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,8 +3,8 @@ let score = "33abc"
 console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 let isloogedIn = "hites"
 
 let booleanIsLoggedIn = Boolean(isloogedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //"" => false
@@ -25,5 +25,33 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let srtingNu = String(someNumber)
-console.log(srtingNu);
-console.log(typeof srtingNu)
+//console.log(srtingNu);
+//console.log(typeof srtingNu)
+
+
+// ***********************  Operations ******************
+
+// console.log(2+3);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "nikhil"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 3);
+// console.log(1 + "3");
+// console.log("1" + 2 + 3);
+// console.log(1 + 3 + "3");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
