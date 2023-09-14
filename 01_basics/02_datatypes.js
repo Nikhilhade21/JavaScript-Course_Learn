4a9fed2494e69c82fcc1c39680816f24a3585e1d
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-14 11:05:59 +0000

    data type

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readiblity should be heigh
+console.log("nik")
+
+let name = "nikhil"
+let age = 19
+let isLogin = false
+
+//string => nikhil
+//number => 2 to power 53
+//boolean => true/false
+//bigint =>  54685485248555954158894n;
+//undefined => not assign value now
+//null => standaline value
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
