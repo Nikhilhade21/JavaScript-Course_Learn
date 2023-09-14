ab818bae1231abed65d7d21a823b67cc9413b141
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-14 14:47:25 +0000

    data-type-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+//primitive
+
+//7 : string, number, Boolean, null, undefined, Symbol, Bigint
+
+const score = 100  
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber =3452154544445676354n;
+
+//Reference (non-primitive type)
+
+//Array, Objects, Function
+
