61077d8217f978cca8ea5ed7dfcd956f9cd2ffad
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-14 16:00:10 +0000

    change 1

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -9,10 +9,10 @@ const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
-const id = symbol('123')
-const anotherId = symbol('123')
+// const id = symbol('123')
+// const anotherId = symbol('123')
 
-console.log(id === anotherId)
+//console.log(id === anotherId)
 
 const bigNumber =3452154544445676354n;
 
@@ -20,3 +20,24 @@ const bigNumber =3452154544445676354n;
 
 //Array, Objects, Function
 
+// ======================================= memory =========================
+
+//stack (primitive datatype), Heap (Non-premitive datatype)
+
+let myYoutubeName = "Rahul"
+let anotherName = myYoutubeName
+anotherName = "chaiurcode"
+
+console.log(myYoutubeName);
+console.log(anotherName)
+
+let userone = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userone
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userone.email);
+console.log(userTwo.email)
