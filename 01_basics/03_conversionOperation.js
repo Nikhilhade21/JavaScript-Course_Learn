f41878e80d11613829c4e79c2bfd6a53e4cdf37b
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-14 11:27:10 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33aas" => NaN
+// true => 1; false => 0
+
+
+let isloogedIn = "hites"
+
+let booleanIsLoggedIn = Boolean(isloogedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "something" => true
+
+
+let someNumber = 33
+
+let srtingNu = String(someNumber)
+console.log(srtingNu);
+console.log(typeof srtingNu)
