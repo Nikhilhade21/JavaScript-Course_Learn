58af151128c970710e769be1011a6eb069d893f1
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-17 20:24:48 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while
+
+// let index = 0
+// while(index < 10) {
+//     console.log(index);
+//     index = index + 2
+// }
+
+// let index1 = ["Flash", "Batman", "Superman"]
+// let i = 0
+// while(i < index1.length) {
+//     console.log(`value is ${index1[i]}`);
+//     i = i + 1
+// }
+
+let score = 1;
+
+do{
+    console.log(`score is ${score}`);
+    score++
+}
+while(score <= 10)
