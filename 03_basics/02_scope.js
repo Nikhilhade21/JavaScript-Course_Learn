5d42dad332269941cfa52e3f3547e2894702a4b2
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-17 14:13:23 +0000

    mini hoting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,8 +5,32 @@ if(true){
     let a = 10;
     const b = 20;
     var c = 30;
-    console.log("inner",a) //local scope
+   // console.log("inner",a) //local scope
 } 
 
-console.log(a)
+// console.log(a)
 
+function one(){
+    const name = "nikhil"
+    function two(){
+        const website = "nik.com"
+        console.log(website, name);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+
+
+
+function addone(value){
+    return value + 1
+}
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
+console.log(addone(5))
