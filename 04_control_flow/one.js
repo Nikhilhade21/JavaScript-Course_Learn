4e23641ee2dcb637d649e92713c1c429b8559646
Nikhil Hade <120098736+Nikhilhade21@users.noreply.github.com>
2023-09-17 18:27:04 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+//if
+
+// if(true){
+
+// }
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`user power ${power}`);
+// }
+
+// console.log(`user power ${power}`)
+
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("test2"); 
+
+// if(balance < 500){
+//     console.log("less than 500")
+// }else if (balance < 750){
+//     console.log("less than 750")
+// }else if (balance < 900){
+//     console.log("less than 900")
+// }else {
+//     console.log("less than 1200")
+// }
+
+const debitCard = true
+const loggedinfromEmail = true
+const loggedinfromGoogle = true
+
+if (loggedinfromEmail &&  debitCard && 2 == 2){
+    console.log("you are allowed")
+}
+
+if (loggedinfromEmail || loggedinfromEmail || 2 == 3){
+    console.log("you are allowed");
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,39 @@
+const userEmail = "nik@gmail.com"
+
+// if(userEmail) {
+//     console.log("yes")
+// }else{
+//     console.log("no");
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+if (userEmail.length === 0){
+    console.log("Array is empty");
+}
+// const empObj = {}
+
+// if(Object.keys(empObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+//nullish Coalescing operator (??): null undefined
+
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = undefined ?? 10 ?? 20
+
+// console.log(val1);
+
+// // terniary operator
+
+// // condition ? true : false
+
+// const iceTeaPrice = 100;
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
